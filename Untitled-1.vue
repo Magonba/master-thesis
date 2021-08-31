18cf3bbc1250d79178ec9ccb60c86f8764be1f1a
Magonba <magonba@protonmail.com>
2021-08-31 11:20:22 +0200

    update

---
+++ Untitled-1.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+   {{ this.mystring }}
+  </div>
+</template>
+<script>
+export default {
+  data() {
+   return {
+    mystring: "Welcome to VueJS",
+   };
+  },
+};
+</script>
